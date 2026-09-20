886916f7f7e6ca0ada37982c2e099200b2eb6dfb
jackwsanderson2 <jsand891@aol.co.uk>
2026-09-20 22:44:02 +0100

    Add files via upload

---
+++ _test_raw_template.py
@@ -0,0 +1,97 @@
+import re
+import pytest
+from pathlib import Path
+from testing_methods import GetRawVariables, Checks
+
+# inputs
+raw_path = Path(r"C:\github\mi-team-byof\Shared\ManagementInformationBYOF\notebooks\raw\nb_raw_bi0076_osa.ipynb")
+
+# global variables
+raw_file_name = raw_path.stem
+raw_tbl_name = GetRawVariables.get_tbl_name(raw_path)
+raw_source_name = GetRawVariables.get_source_file(raw_path)
+raw_adls_account = GetRawVariables.get_adls_account(raw_path)
+raw_container = GetRawVariables.get_container(raw_path)
+raw_mode = str(GetRawVariables.get_mode(raw_path)).lower()
+raw_highwatermark_column = GetRawVariables.get_highwatermark_column(raw_path)
+
+# RawTests
+def test_raw_case_type():
+    """
+    Checks if table name is in snake case.
+    """
+    assert Checks.case_type(raw_tbl_name) == 'snake_case',(
+        f"Table: {raw_tbl_name}"
+        )
+
+def test_raw_tbl_naming():
+    """
+    Checks the prefix of table name is 'tbl_raw_'.
+    """
+    assert Checks.tbl_raw_naming(raw_tbl_name) is True,(
+        f"Table: {raw_tbl_name}. Prefix should be 'tbl_raw_'"
+        )
+
+def test_raw_bi_number_extract():
+    """
+    Checks bi number is in format bi0000
+    """
+    assert re.fullmatch(r"bi\d{4}",Checks.bi_number_extract(raw_tbl_name)), (
+        f"Table: {raw_tbl_name}"
+        )
+
+def test_raw_bi_tbl_file_consistency():
+    """
+    Test for consistency between bi numbers of table name and file name.
+    """
+    assert Checks.tbl_file_consistency(Checks.bi_number_extract,raw_tbl_name,raw_file_name) is True, (
+        f"Table: {raw_tbl_name}, File: {raw_file_name}"
+        )
+
+def test_raw_name_tbl_file_consistency():
+    """
+    Test for consistency between name labels of table name and file name.
+    """
+    assert Checks.tbl_file_consistency(Checks.name_extract,raw_tbl_name,raw_file_name) is True, (
+        f"Table: {raw_tbl_name}, File: {raw_file_name}"
+        )
+
+def test_raw_source_file_ending():
+    """
+    Test if source file ends with '/'.
+    """
+    assert Checks.source_file_ending(raw_source_name) is True, (
+        f"Source File: {raw_source_name}"
+        )
+
+def test_raw_adls_account():
+    """
+    Test if adls account is in format 'aznednaprodl01'.
+    """
+    assert raw_adls_account == 'aznednaprodl01', (
+        f"ADLS Account: {raw_adls_account}"
+        )
+
+def test_raw_container():
+    """
+    Test if container is in format 'raw'.
+    """
+    assert raw_container == 'self-serve', (
+        f"Container: {raw_container}"
+        )
+
+def test_raw_mode():
+    """
+    Test if mode is in format 'append'.
+    """
+    assert raw_mode == 'incremental' or raw_mode == 'overwrite', (
+        f"Mode: {raw_mode}"
+        )
+
+def test_raw_highwatermark_column():
+    """
+    Test if highwatermark column is in format 'highwatermark'.
+    """
+    assert raw_highwatermark_column == 'load_id', (
+        f"Highwatermark Column: {raw_highwatermark_column}"
+        )
---
+++ testing_methods.py
@@ -0,0 +1,299 @@
+import nbformat
+import re
+import yaml
+from pathlib import Path
+
+
+class GetRawVariables:
+    @staticmethod
+    def get_variable(notebook_path, variable_name):
+        nb = str(nbformat.read(notebook_path, as_version=4))
+
+        pattern = rf"{variable_name}\s*=\s*['\"]?([^'\"\s]+)"
+        match = re.search(pattern, nb)
+
+        return match.group(1) if match else None
+
+    @staticmethod
+    def get_adls_account(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "adls_account"
+        )
+
+    @staticmethod
+    def get_container(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "container"
+        )
+
+    @staticmethod
+    def get_source_file(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "source_file"
+        )
+
+    @staticmethod
+    def get_catalog(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "catalog ="
+        )
+
+    @staticmethod
+    def get_schema(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "schema"
+        )
+
+    @staticmethod
+    def get_tbl_name(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "tbl_name"
+        )
+
+    @staticmethod
+    def get_ext_loc(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "ext_loc"
+        )
+
+    @staticmethod
+    def get_mode(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "mode"
+        )
+
+    @staticmethod
+    def get_highwatermark_column(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "highwatermark_column"
+        )
+
+    @staticmethod
+    def get_mandatory_columns(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "mandatory_columns"
+        )
+
+    @staticmethod
+    def get_clustering_columns(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "clustering_columns"
+        )
+
+    @staticmethod
+    def get_df(notebook_path):
+        return GetRawVariables.get_variable(
+            notebook_path,
+            "df"
+        )
+
+class GetConformedVariables:
+    @staticmethod
+    def get_variable(notebook_path, variable_name):
+        nb = str(nbformat.read(notebook_path, as_version=4))
+
+        pattern = rf"{re.escape(variable_name)}\s*(?:=|\.)\s*['\"]?([^'\"\s]+)"
+
+        match = re.search(pattern, nb)
+
+        return match.group(1) if match else None
+
+    @staticmethod
+    def get_source_table(notebook_path):
+        return GetConformedVariables.get_variable(
+            notebook_path,
+            "SOURCE_TABLE"
+        )
+
+    @staticmethod
+    def get_target_table(notebook_path):
+        return GetConformedVariables.get_variable(
+            notebook_path,
+            "TARGET_TABLE"
+        )
+
+    @staticmethod
+    def get_class_name(notebook_path):
+        return GetConformedVariables.get_variable(
+            notebook_path,
+            "class"
+        )
+
+class GetReportingVariables:
+    @staticmethod
+    def get_variable(notebook_path, variable_name):
+        nb = str(nbformat.read(notebook_path, as_version=4))
+
+        pattern = rf"{re.escape(variable_name)}\s*(?:=|\.)\s*['\"]?([^'\"\s]+)"
+
+        match = re.search(pattern, nb)
+
+        return match.group(1) if match else None
+
+    @staticmethod
+    def get_view_name(notebook_path):
+        return GetReportingVariables.get_variable(
+            notebook_path,
+            "CREATE OR REPLACE VIEW {catalog}.`{env}-ds-mgmtinfo-neu-dbr-01`"
+        )
+
+    @staticmethod
+    def get_division_filter(notebook_path):
+        nb = str(nbformat.read(notebook_path, as_version=4))
+        return 'DIV-16' in nb or 'DIV-15' in nb
+            
+class GetJobVariables:
+    # command: py -m pip install pyyaml
+     
+    @staticmethod
+    def get_yaml(job_path):
+        with open(job_path) as f:
+            data = yaml.safe_load(f)
+
+        jobs = data.get("resources", {}).get("jobs", {})
+
+        for job_id, job in jobs.items():
+            print(f"Job: {job.get('name')}")
+
+            for task in job.get("tasks", []):
+                print(f"  Task: {task.get('task_key')}")
+
+                notebook = task.get("notebook_task")
+                if notebook:
+                    print(f"    Notebook: {notebook.get('notebook_path')}")
+
+    @staticmethod
+    def get_job_data(job_path):
+        with open(job_path) as f:
+            data = yaml.safe_load(f)
+        
+        return data.get("resources", {}).get("jobs", {})
+
+    @staticmethod
+    def get_task_data(job_path, task_key):
+        data = GetJobVariables.get_job_data(job_path)
+
+        tasks_lookup = {
+            task["task_key"]: task
+            for task in data["create_views"]["tasks"]
+        }
+
+        return tasks_lookup[task_key]
+    
+class Checks ():
+    def __init__(self):
+        self
+
+    def case_type(tbl_name):
+        """
+        Detects case type.
+        """
+
+        tbl_name = tbl_name.replace(" ", "")
+
+        SNAKE_CASE = re.compile(r"^[a-z][a-z0-9_]*$")
+        PASCAL_CASE = re.compile(r"^[A-Z][a-zA-Z0-9]*$")
+        CAMEL_CASE = re.compile(r"^[a-z]+(?:[A-Z][a-z0-9]*)*$")
+        
+        if bool(SNAKE_CASE.match(tbl_name)) == True:
+            return 'snake_case'
+        elif bool(PASCAL_CASE.match(tbl_name)) == True:
+            return 'pascal case'
+        elif bool(CAMEL_CASE.match(tbl_name)) == True:
+            return 'camel case'
+
+    def tbl_raw_naming(tbl_name):
+        """
+        Checks variable starts with prefix 'tbl_raw_'.
+        """
+
+        prefix = 'tbl_raw_'
+        if tbl_name.startswith(prefix):
+            return True
+        else:
+            return False
+
+    def tbl_conformed_naming(tbl_name):
+        """
+        Checks variable starts with prefix 'tbl_conformed_'.
+        """
+
+        prefix = 'tbl_conformed_'
+        if tbl_name.startswith(prefix):
+            return True
+        else:
+            return False
+
+    def vw_reporting_naming(tbl_name):
+        """
+        Checks variable starts with prefix 'vw_reporting_'.
+        """
+
+        prefix = 'vw_reporting_'
+        if tbl_name.startswith(prefix):
+            return True
+        else:
+            return False
+
+    def bi_number_extract(input):
+        """
+        Extracts bi number in format bi0000.
+        """
+        search_term = 'bi'
+        position = input.find(search_term)
+        bi_number = input[position:position+6]
+
+        return bi_number
+
+    def name_extract(input):
+        """
+        Extracts name of data.
+        """
+        search_term = 'bi'
+        position = input.find(search_term)
+        name = input[position+7:]
+
+        return name
+
+    def view_extract(input):
+            """
+            Extracts name of data.
+            """
+            search_term = 'vw_reporting_'
+            position = input.find(search_term)
+            name = input[position+11:]
+    
+            return name
+
+    def tbl_file_consistency(function,tbl_name,file_name):
+        """
+        Checks naming consistency between table and file.
+        """
+        if function((tbl_name)) == function((file_name)):
+            return True
+        else:
+            return False
+
+    def source_file_ending(source_file):
+        """
+        Checks URL link ends in '/'.
+        """
+        if source_file.endswith('/'):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def get_country(variable_name):
+        return variable_name.rsplit("_", 1)[1]
